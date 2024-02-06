cf02912bb0d64bf090aeefd94794fdb3b246f431
Amiano-Abreu <moseyweb@gmail.com>
2024-02-06 07:15:49 +0000

    added new font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { Roboto_Slab } from "next/font/google";
+
+const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap"
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${robotoSlab.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
