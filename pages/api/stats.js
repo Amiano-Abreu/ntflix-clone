e904707c7ca7521172d30c546262b7579d791f85
Amiano-Abreu <moseyweb@gmail.com>
2024-03-02 00:32:00 +0000

    new stats api, added verification of JWT token.

---
+++ pages/api/stats.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+
+export default async function stats(req, res) {
+    if (req.method === "POST") {
+
+        try {
+            const token = req.cookies.token
+            if (!token) {
+                res.status(403).send({})
+            } else {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                res.send({})
+            }
+        } catch (error) {
+            console.error("Error occurred /stats", error)
+            res.status(500).send({
+                done: false,
+                error: error?.message
+            })
+        } 
+    }
+}
