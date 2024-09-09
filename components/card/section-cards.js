f13d3be0705192ef0de5e15a94dade808f6ec72a
Amiano-Abreu <moseyweb@gmail.com>
2024-09-09 18:56:24 +0100

    section cards update, login styles.

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -4,33 +4,39 @@ import Card from "./card";
 import styles from "./section-cards.module.css"
 
 const SectionCards = ({ title, videos = [], size, shouldWrap = false, shouldScale }) => {
-    return (
-        <section className={styles.container}>
-            <h2 className={styles.title}>
-                {title}
-            </h2>
-            <div className={`${styles.cardWrapper} ${shouldWrap ? styles.wrap : ""}`}>
-                {
-                    videos?.map( (video, i) => {
-                            return (
-                                <Link
-                                    key={i}
-                                    href={`/video/${video.id}`}
-                                >
-                                    <Card
-                                        imgUrl={video.imgUrl}
-                                        size={size}
-                                        id={i}
-                                        shouldScale={shouldScale}
-                                    />
-                                </Link>
-                            )
-                        }
-                    )
-                }
-            </div>
-        </section>
-    );
+    let sectionCards = <></>
+
+    if ( videos && videos.length !== 0 ) {
+        
+        sectionCards = <section className={styles.container}>
+                            <h2 className={styles.title}>
+                                {title}
+                            </h2>
+                            <div className={`${styles.cardWrapper} ${shouldWrap ? styles.wrap : ""}`}>
+                                {
+                                    videos?.map( (video, i) => {
+                                            return (
+                                                <Link
+                                                    key={i}
+                                                    href={`/video/${video.id}`}
+                                                >
+                                                    <Card
+                                                        imgUrl={video.imgUrl}
+                                                        size={size}
+                                                        id={i}
+                                                        shouldScale={shouldScale}
+                                                    />
+                                                </Link>
+                                            )
+                                        }
+                                    )
+                                }
+                            </div>
+                        </section>
+
+    }
+    
+    return sectionCards;
 }
 
 export default SectionCards;
